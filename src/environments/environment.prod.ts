becfcf1145b033f332076eec5fd9b6de57b0b32a
Nuno Oliveira <nunooliveira8@gmail.com>
2026-03-22 21:01:30 +0000

    chore(config): update production api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // Arquivo de configuração para produção
 export const environment = {
   production: true,
-  apiUrl: 'https://sm-health-core.onrender.com'
+  //apiUrl: 'https://sm-health-core.onrender.com'
+  apiUrl: 'https://ttvkqywsxlbm.eu-central-1.clawcloudrun.com'
 };
